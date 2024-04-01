9293554a52f256fbdfe60a6159d666a5e1f3ef80
kimminam <bluemind8520@gmail.com>
2024-04-01 23:39:56 +0900

    Button Components 추가

--- study-app/src/App.jsx
+++ study-app/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
 import Main from './components/Main'
+import Button from './components/Button'
 
 function App() {
   return (
     <>
     <Main/>
+    <Button text={"메일"} color={"red"}/> 
+    <Button text={"카페"} /> 
+    <Button text={"블로그"}>
+      <div>자식요소</div>
+    </Button>
     </>
   )
 }
