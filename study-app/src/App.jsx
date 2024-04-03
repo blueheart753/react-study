ebcf53e32b7a376f3bc72511b96ea92a69f128c1
kimminam <bluemind8520@gmail.com>
2024-04-03 22:33:55 +0900

    State에 관한 코드

--- study-app/src/App.jsx
+++ study-app/src/App.jsx
@@ -1,10 +1,23 @@
-import './App.css';
-import { useState } from 'react';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const state = useState(0);
-  console.log(state);
-  return <main></main>;
+  const [count, setCount] = useState(0)
+  const [light, setLight] = useState('Off')
+  return (
+    <main>
+      <div>
+        <h1>{light}</h1>
+        <button onClick={() => setLight(light == 'On' ? 'Off' : 'On')}>
+          {light == 'On' ? 'Off' : 'On'}
+        </button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
