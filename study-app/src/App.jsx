5445f5bfe34d30674f641bd66b0c88709a571ce8
kimminam <bluemind8520@gmail.com>
2024-04-03 19:35:03 +0900

    State

--- study-app/src/App.jsx
+++ study-app/src/App.jsx
@@ -1,18 +1,10 @@
-import './App.css'
-import Main from './components/Main'
-import Button from './components/Button'
+import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <>
-    <Main/>
-    <Button text={"메일"} color={"red"}/> 
-    <Button text={"카페"} /> 
-    <Button text={"블로그"}>
-      <div>자식요소</div>
-    </Button>
-    </>
-  )
+  const state = useState(0);
+  console.log(state);
+  return <main></main>;
 }
 
-export default App
+export default App;
