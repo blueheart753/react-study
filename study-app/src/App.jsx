9a90bfbcbfea193430aaf041ddfdc4c69d54dcf9
kimminam <bluemind8520@gmail.com>
2024-04-05 15:48:10 +0900

    리렌더링 Bulb, Counter 컴포넌트 Import

--- study-app/src/App.jsx
+++ study-app/src/App.jsx
@@ -1,23 +1,13 @@
-import './App.css'
-import { useState } from 'react'
-
+import './App.css';
+import Counter from './components/Counter';
+import Bulb from './components/Bulb';
 function App() {
-  const [count, setCount] = useState(0)
-  const [light, setLight] = useState('Off')
   return (
     <main>
-      <div>
-        <h1>{light}</h1>
-        <button onClick={() => setLight(light == 'On' ? 'Off' : 'On')}>
-          {light == 'On' ? 'Off' : 'On'}
-        </button>
-      </div>
-      <div>
-        <h1>{count}</h1>
-        <button onClick={() => setCount(count + 1)}>+</button>
-      </div>
+      <Bulb />
+      <Counter />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
