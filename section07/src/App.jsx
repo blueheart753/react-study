91e87578b1b09f039dd4debe15219fd7d80e6d8f
kimminam <bluemind8520@gmail.com>
2024-04-28 22:43:23 +0900

    useEffect hook 사용해보기

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,18 +1,34 @@
-import "./App.css";
-import Viewer from "./components/Viewer";
-import Controller from "./components/Controller";
-import { useState } from "react";
+import './App.css'
+import Viewer from './components/Viewer'
+import Controller from './components/Controller'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
+  const [input, setInput] = useState('')
 
-  const onClickButton = (value) => {
-    setCount(count + value);
-  };
+  useEffect(() => {
+    console.log(`count : ${count} / input : ${input}`)
+  }, [count, input])
+  // 의존성 배열
+  // dependency array
+  // deps
+  const onClickButton = value => {
+    setCount(count + value)
+  }
 
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <section>
+        <input
+          type="text"
+          value={input}
+          onChange={e => {
+            setInput(e.target.value)
+          }}
+        />
+      </section>
       <section>
         <Viewer count={count} />
       </section>
@@ -20,7 +36,7 @@ function App() {
         <Controller onClickButton={onClickButton} />
       </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
