62eecac0fb81fc5a28033ff9ba9dd7f2d25acf53
kimminam <bluemind8520@gmail.com>
2024-04-29 21:17:08 +0900

    section08 project settings

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,38 +1,37 @@
-import './App.css';
-import Viewer from './components/Viewer';
-import Controller from './components/Controller';
-import Even from './components/Even';
-import { useState, useEffect, useRef } from 'react';
+import './App.css'
+import Viewer from './components/Viewer'
+import Controller from './components/Controller'
+import Even from './components/Even'
+import { useState, useEffect, useRef } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [input, setInput] = useState('');
-  // const isMount = useRef(false);
-  const [isMount, setIsMount] = useState(false);
+  const [count, setCount] = useState(0)
+  const [input, setInput] = useState('')
+  const isMount = useRef(false)
+  // const [isMount, setIsMount] = useState(false);
 
   // Life Cycle의 순서
   // 1. Mount : 탄생 (컴포넌트가 처음 랜더링된 상태)
   useEffect(() => {
-    console.log('mount');
-  }, []);
+    console.log('mount')
+  }, [])
   // 2. Update : 변화, 리랜더링 (컴포넌트가 변화하거나 리랜더링된 상태)
-  // useEffect(() => {
-  //   if (!isMount.current) {
-  //     isMount.current = true;
+  useEffect(() => {
+    if (!isMount.current) {
+      isMount.current = true
+      return
+    }
+    console.log('update')
+  })
+
+  // const stateTest = () => {
+  //   if (!isMount) {
+  //     setIsMount(true);
   //     return;
   //   }
   //   console.log('update');
-  // });
-
-  const stateTest = () => {
-    if (!isMount) {
-      setIsMount(true);
-      return;
-    }
-    console.log('update');
-  };
-
-  stateTest();
+  // };
+  // stateTest();
 
   // 3. Unmount : 죽음 (컴포넌트가 완전히 닫힌 상태)
 
@@ -40,8 +39,8 @@ function App() {
   // dependency array
   // deps
   const onClickButton = value => {
-    setCount(count + value);
-  };
+    setCount(count + value)
+  }
 
   return (
     <div className="App">
@@ -51,7 +50,7 @@ function App() {
           type="text"
           value={input}
           onChange={e => {
-            setInput(e.target.value);
+            setInput(e.target.value)
           }}
         />
       </section>
@@ -63,7 +62,7 @@ function App() {
         <Controller onClickButton={onClickButton} />
       </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
