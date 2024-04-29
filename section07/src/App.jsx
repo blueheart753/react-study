7933c8868c43d26d41814980224d0fcb6d5f0460
kimminam <bluemind8520@gmail.com>
2024-04-29 20:55:00 +0900

    useEffect 예제 및 실습

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,21 +1,47 @@
-import './App.css'
-import Viewer from './components/Viewer'
-import Controller from './components/Controller'
-import { useState, useEffect } from 'react'
+import './App.css';
+import Viewer from './components/Viewer';
+import Controller from './components/Controller';
+import Even from './components/Even';
+import { useState, useEffect, useRef } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [input, setInput] = useState('')
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState('');
+  // const isMount = useRef(false);
+  const [isMount, setIsMount] = useState(false);
 
+  // Life Cycle의 순서
+  // 1. Mount : 탄생 (컴포넌트가 처음 랜더링된 상태)
   useEffect(() => {
-    console.log(`count : ${count} / input : ${input}`)
-  }, [count, input])
+    console.log('mount');
+  }, []);
+  // 2. Update : 변화, 리랜더링 (컴포넌트가 변화하거나 리랜더링된 상태)
+  // useEffect(() => {
+  //   if (!isMount.current) {
+  //     isMount.current = true;
+  //     return;
+  //   }
+  //   console.log('update');
+  // });
+
+  const stateTest = () => {
+    if (!isMount) {
+      setIsMount(true);
+      return;
+    }
+    console.log('update');
+  };
+
+  stateTest();
+
+  // 3. Unmount : 죽음 (컴포넌트가 완전히 닫힌 상태)
+
   // 의존성 배열
   // dependency array
   // deps
   const onClickButton = value => {
-    setCount(count + value)
-  }
+    setCount(count + value);
+  };
 
   return (
     <div className="App">
@@ -25,18 +51,19 @@ function App() {
           type="text"
           value={input}
           onChange={e => {
-            setInput(e.target.value)
+            setInput(e.target.value);
           }}
         />
       </section>
       <section>
         <Viewer count={count} />
+        {count % 2 == 0 ? <Even /> : null}
       </section>
       <section>
         <Controller onClickButton={onClickButton} />
       </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
